8e7b20b35008cd9a258cc8b80347e959730acab7
Mauro Garro <maurogarro73@gmail.com>
2023-06-15 01:24:33 -0300

    cart views funcionando

--- src/routes/homeCarts.router.js
+++ src/routes/homeCarts.router.js
@@ -5,14 +5,23 @@ export const cartsHtml = express.Router();
 
 cartsHtml.get('/:cid', async (req, res) => {
   try {
-    const cid = req.params.cid;
-    const cart = await CartModel.findOne({ _id: cid }).populate('products.product');
-    const idCart = cart._id;
+    const { cid } = req.params;
+    const cartFound = await CartModel.findById(cid).populate('products.product');
+    if (!cartFound) {
+      throw new Error('Cart not found');
+    }
 
-    console.log('Cart', cart);
-    console.log('Product', cart.products);
+    const idCart = cartFound._id;
 
-    return res.status(200).render('carts', { cart, idCart });
+    let cart = cartFound.products.map((item) => {
+      return {
+        title: item._id.title,
+        price: item._id.price,
+        quantity: item.quantity,
+      };
+    });
+
+    return res.status(200).render('carts', { cart: cart, idCart });
   } catch (error) {
     console.log(error);
   }
